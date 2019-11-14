aff72d3219d8713cf0082c47cae4f6b583e1ca41
Amani Singh Vohra <asv6ef@virginia.edu>
2019-11-13 20:44:28 -0500

    Basic Stack Navigator
    HomePage, Restaurant, Reviews

--- wafoodwa/App.js
+++ wafoodwa/App.js
@@ -1,19 +1,81 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return <MyNavigator />;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class HomePage extends React.Component {
+  static navigationOptions = {
+    headerTitle: 'WaFoodWa',
+  };
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+        }}>
+        <Button
+          title="Resturant"
+          onPress={() => this.props.navigation.navigate('Resturant')}
+        />
+      </View>
+    );
+  }
+}
+
+class Resturant extends React.Component {
+  static navigationOptions = {
+    headerTitle: 'Resturant',
+  };
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+        }}>
+        <Button
+          title=" Resturant Reviews"
+          onPress={() =>
+            this.props.navigation.navigate('Reviews')
+          }
+        />
+      </View>
+    );
+  }
+}
+
+class Reviews extends React.Component {
+  static navigationOptions = {
+    headerTitle: 'Reviews',
+  };
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+        }}>
+        <Button
+          title="Go back to HomePage"
+          onPress={() =>
+            this.props.navigation.navigate('HomePage')
+          }
+        />
+      </View>
+    );
+  }
+}
+
+const MyNavigator = createStackNavigator(
+  {
+    HomePage: HomePage,
+    Resturant: Resturant,
+    Reviews: Reviews,
   },
-});
+);
